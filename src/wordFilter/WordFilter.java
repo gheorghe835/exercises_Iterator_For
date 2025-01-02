4d33cf1366ee2a73ab3301e5e55212c385d0a3f8
gheorghe835 <gheorghe835@gmail.com>
2025-01-02 08:35:36 +0200

    adaugarea exercitiului Eliminarea elementelor folosind Iterator. crearea unui iterator pentru a parcurge lista de cuvinte

--- src/wordFilter/WordFilter.java
+++ src/wordFilter/WordFilter.java
@@ -1,4 +1,11 @@
 package wordFilter;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class WordFilter {
+    public static void deleteShortWords(List<String> words){
+        //crearea unui iterator pentru a parcurge lista de cuvinte
+        Iterator<String> iterator = words.iterator();
+    }
 }
