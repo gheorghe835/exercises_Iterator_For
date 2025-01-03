2c7b170f0bb74badbee68b9921c6cd9c99fea99b
gheorghe835 <gheorghe835@gmail.com>
2025-01-03 16:59:02 +0200

    adaugarea Harta pentru nume si virsta, Metoda de adaugare a unei persoane, Metoda de afisarea persoanelor, Metoda de eliminare a persoanelor

---
+++ src/nameExistence/PeopleManager.java
@@ -0,0 +1,38 @@
+package nameExistence;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class PeopleManager {
+    //Harta pentru nume si virsta
+    private HashMap<String,Integer> people;
+
+    //Constructor
+
+    public PeopleManager() {
+        people = new HashMap<>();
+    }
+
+    //Metoda de adaugare a unei persoane
+    public void addPerson(String name,int age){
+        people.put(name,age);
+    }
+
+    //Metoda de afisarea persoanelor
+    public void showPeople(){
+        System.out.print("  Persoanele di harta:: " + people);
+    }
+
+    //Metoda de eliminare a persoanelor
+    public void deletePerson(){
+        Iterator<Map.Entry<String,Integer>> iterator = people.entrySet().iterator();
+
+        while (iterator.hasNext()){
+            Map.Entry<String,Integer> entry = iterator.next();
+            if (entry.getValue() < 18){
+                iterator.remove();
+            }
+        }
+    }
+}
