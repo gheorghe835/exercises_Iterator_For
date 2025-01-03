ff40abd8b8426be2b6f1174cce127263fa78dce1
gheorghe835 <gheorghe835@gmail.com>
2025-01-03 08:34:05 +0200

    adaugarea tascului Folosește un HashMap care stochează numele și vârstele unor persoane.

---
+++ src/nameExistence/Print.java
@@ -0,0 +1,10 @@
+package nameExistence;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("  4. Parcurgerea unei hărți\n" +
+                "Folosește un HashMap care stochează numele și vârstele unor persoane.\n" +
+                "•\tParcurge perechile cheie-valoare folosind un Iterator.\n" +
+                "•\tElimină din hartă persoanele cu vârsta mai mică de 18 ani.\n");
+    }
+}
